e03b79ad210725d55ec1e0e31f55fbcbb1a858c3
Orkuncuk <123820259+Orkuncuk@users.noreply.github.com>
2023-11-23 19:50:46 +0300

    a

---
+++ Java_projects/src/temel_seviye/odev.java
@@ -0,0 +1,21 @@
+package temel_seviye;
+import java.util.Scanner;
+public class odev {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double kenara, kenarb, kenarc;
+        System.out.print("1. Kenarı giriniz: ");
+        kenara = input.nextDouble();
+        System.out.print("2. Kenarı giriniz: ");
+        kenarb = input.nextDouble();
+        System.out.print("3. Kenarı giriniz: ");
+        kenarc = input.nextDouble();
+        double u = (kenara + kenarb + kenarc) / 2;
+        double alan = Math.sqrt(u * (u - kenara) * (u - kenarb) * (u - kenarc));
+        System.out.println("Üçgenin alanı: " + alan);
+        input.close();
+
+        
+    }
+    
+}

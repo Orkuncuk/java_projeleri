908940216d46eee897bd7bd76a10c5f690a8505d
Orkuncuk <123820259+Orkuncuk@users.noreply.github.com>
2023-11-23 17:35:57 +0300

    deneme1

---
+++ Java_projects/src/temel_seviye/NotHesaplama.java
@@ -0,0 +1,35 @@
+package temel_seviye;
+import java.util.Scanner;
+
+public class NotHesaplama {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int matematik, fizik, kimya, turkce, tarih, muzik;
+
+        System.out.print("Matematik notunuz: ");
+        matematik = input.nextInt();
+
+        System.out.println("Fizik notunuz: ");
+        fizik = input.nextInt();
+        
+        System.out.println("Kimya notunuz: ");
+        kimya = input.nextInt();
+
+        System.out.println("Turkce notunuz: ");
+        turkce = input.nextInt();
+
+        System.out.println("Tarih notunuz: ");
+        tarih = input.nextInt();
+
+        System.out.println("Muzik notunuz: ");
+        muzik = input.nextInt();
+
+        input.close();
+
+        int ortalama = (matematik + fizik + kimya + turkce + tarih + muzik) / 6;
+        boolean kosul = ortalama >= 60;
+        String sonuc = kosul ? "Sınıfı Geçti" : "Sınıfta Kaldı";
+        System.out.println(sonuc);
+
+    }
+}
